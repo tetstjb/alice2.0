647f905f2ce8a2622a230b45c2f4f71f249d2206
Smaugopp <163888260+Smaugopp@users.noreply.github.com>
2024-08-15 16:11:31 +0530

    Update config.py
    Signed-off-by: Smaugopp <163888260+Smaugopp@users.noreply.github.com>

--- config.py
+++ config.py
@@ -78,10 +78,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/9e2e83ad4bcd814a538c5.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/adb4c3fe1124461b023c4.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/531090c47c12e983bf731.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
