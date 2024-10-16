9d7f0eac1969ce96abeaf964a77f72813b3a40ff
Smaugopp <163888260+Smaugopp@users.noreply.github.com>
2024-10-17 02:27:44 +0530

    Update broadcast.py
    Signed-off-by: Smaugopp <163888260+Smaugopp@users.noreply.github.com>

--- AnonXMusic/plugins/misc/broadcast.py
+++ AnonXMusic/plugins/misc/broadcast.py
@@ -13,8 +13,8 @@ from AnonXMusic.utils.database import (
     get_served_chats,
     get_served_users,
 )
-from AviaxMusic.utils.decorators.language import language
-from AviaxMusic.utils.formatters import alpha_to_int
+from AnonXMusic.utils.decorators.language import language
+from AnonXMusic.utils.formatters import alpha_to_int
 from config import adminlist
 
 IS_BROADCASTING = False
